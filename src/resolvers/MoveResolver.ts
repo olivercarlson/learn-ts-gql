0d2b15817a4d160d70b3b1d730600f46b241b45a
Oliver Carlson <oswain@gmail.com>
2020-10-11 08:36:17 -0700

    CRUD complete. finished tutorial

--- src/resolvers/MoveResolver.ts
+++ src/resolvers/MoveResolver.ts
@@ -1,10 +1,46 @@
-import { Resolver, Mutation, Arg, Int } from 'type-graphql';
+import { Movie } from '../entity/Movie';
+import { Resolver, Mutation, Arg, Int, Query, InputType, Field } from 'type-graphql';
+
+@InputType()
+class MovieInput {
+	@Field()
+	title: string;
+
+	@Field(() => Int)
+	minutes: number;
+}
+
+@InputType()
+class MovieUpdateInput {
+	@Field(() => String, { nullable: true })
+	title?: string;
+
+	@Field(() => Int, { nullable: true })
+	minutes?: number;
+}
 
 @Resolver()
 export class MovieResolver {
+	@Mutation(() => Movie)
+	async createMovie(@Arg('options', () => MovieInput) options: MovieInput) {
+		const movie = await Movie.create(options).save();
+		return movie;
+	}
+
 	@Mutation(() => Boolean)
-	createMovie(@Arg('title', () => String) title: string, @Arg('minutes', () => Int) minutes: number) {
-		console.log(title, minutes);
+	async updateMovie(@Arg('id', () => Int) id: number, @Arg('input', () => MovieUpdateInput) input: MovieUpdateInput) {
+		await Movie.update({ id }, input);
 		return true;
+	}
+
+	@Mutation(() => Boolean)
+	async deleteMovie(@Arg('id', () => Int) id: number) {
+		await Movie.delete({ id });
+		return true;
+	}
+
+	@Query(() => [Movie])
+	movies() {
+		return Movie.find();
 	}
 }
